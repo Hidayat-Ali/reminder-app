b98314457ed08a7845967956d0888ff032347149
Hidayat <meerhidayat563@gmail.com>
2021-07-04 12:28:56 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Hiddhu',
+    age: 18,
+    image:
+      './hidayat.jpeg',
+  },
+  {
+    id: 2,
+    name: 'Mahmood Hussain',
+    age: 24,
+    image:
+      './Mahmood.jpeg',
+  },
+  {
+    id: 3,
+    name: 'Gulshan Abass',
+    age: 12,
+    image:
+      './Abass.jpeg',
+  },
+  {
+    id: 4,
+    name: 'Zafar Mehdi',
+    age: 25,
+    image:
+      './zafar.jpeg',
+  },
+  {
+    id: 5,
+    name: 'Sameey soleimani',
+    age: 27,
+    image:
+      './sameey.jpeg',
+  },
+];
